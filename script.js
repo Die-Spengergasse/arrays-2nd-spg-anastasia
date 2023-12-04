6244760794229c402eb2f6ef670d1e551095da44
Anastasia Brunner <bru210318@spengergasse.at>
2023-12-04 21:28:52 +0100

    commit

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ output.publish = function (arg) {
     this.innerHTML = JSON.stringify(arg);
 };
 
+
 const button_push = document.getElementById("button_push");
 button_push.addEventListener("click", push);
 
@@ -49,87 +50,94 @@ button_spread.addEventListener("click", spread );
 const button_reduce = document.getElementById("button_reduce");
 button_reduce.addEventListener("click", reduce );
 
-function push() {
-    const i1 = eval(input1.value);
-    const i2 = eval(input2.value);
-    i1.push(i2);
-    output.publish(result);
+function push() { //Strings einem Array hinzufügen
+    const i1= eval(input1.value);
+    const i2 =input2.value;
+    const i3 =input3.value;
+    i1.push(i2,i3);
+    output.publish(i1);
+
 }
 
-function indexof() {
+function indexof() { //Zeigt die Stelle an dem I2 in I1 vorkommt wenn I2 nicht vorkommt => -1
     const i1 = eval(input1.value);
     const i2 = eval(input2.value);
     const result = i1.indexOf(i2);
     output.publish(result);
 }
 
-function includes() {
+function includes() { // returns true oder false wenn I2 in I1 vorkommt
     const i1 = eval(input1.value);
     const i2 = eval(input2.value);
     const result = i1.includes(i2);
     output.publish(result);
 }
 
-function slice() {
-    const i1 = eval(input1.value);
-    const i2 = eval(input2.value);
-    const i3 = eval(input3.value);
+function slice() { //returned ausgewählte Elemente in neuem Array
+    const i1 = input1.value;
+    const i2 = parseInt(input2.value);
+    const i3 = parseInt(input3.value);
     const result = i1.slice(i2, i3);
     output.publish(result);
 }
 
-function splice() {
+function splice() { // ersetzt ein Array bestandteil an einer Stelle
     const i1 = eval(input1.value);
-    const i2 = eval(input2.value);
-    const i3 = eval(input3.value);
+    const i2 = parseInt(input2.value);
+    const i3 = parseInt(input3.value);
     i1.splice(i2, i3);
     output.publish(i1);
 }
 
-function of() {
+
+function of() { // eingegebene Strings werden in gemeinsames Array gegeben
     const result = Array.of(input1.value, input2.value, input3.value);
     output.publish(result);
 }
 
-function map() {
+function map() {// i1 ist array und I2 eine mathematische funktion z.B (x)=> x*2 alles im Array wird *2 gerechnet
+        const i1 = eval(input1.value);
+        const i2 = eval(input2.value);
+            const result = i1.map(i2);
+            output.publish(result);
+}
+
+function filter() { //i1= array i2= filter funktion z.B (x)=> x<3 alle Zahlen unter 3 aus dem Array werden ausgegeben
     const i1 = eval(input1.value);
-    const result = i1.map(eval(input2.value));
+    const i2 = input2.value; 
+    const result = i1.filter(eval(i2));
     output.publish(result);
 }
 
-function filter() {
-    const i1 = eval(input1.value);
-    const result = i1.filter(eval(input2.value));
-    output.publish(result);
-}
-
-function split() {
-    const i1 = eval(input1.value);
-    const i2 = eval(input2.value);
+    
+function split() {// i1 ein String mit Trennungszeichen i2= ein Trennungszeichen i1 wird bei Trennungszeichen getrennt und Array gemacht
+    const i1 = input1.value;
+    const i2 = input2.value;
     const result = i1.split(i2);
     output.publish(result);
 }
 
-function join() {
+function join() {// i1=array i2=string zwischen jedem array wird zu string und zwischen jedem element ist der i2
     const i1 = eval(input1.value);
-    const i2 = eval(input2.value);
-    const result = i1.join(i2);
-    output.publish(result);
+    const i2 = input2.value;
+        const result = i1.join(i2);
+        output.publish(result);
 }
 
-function some() {
+
+function some() { // i1= array i2= Bedingung wenn Bendinung auf eines der Elemente im array zutrifft => true
     const i1 = eval(input1.value);
     const result = i1.some(eval(input2.value));
     output.publish(result);
 }
 
-function reverse() {
+function reverse() { //array umgekehrt ausgegeben
     const i1 = eval(input1.value);
     i1.reverse();
     output.publish(i1);
 }
 
-function spread() {
+function spread() {// Eigenschaften eines Arrays werden aufgespaltet und in anderes Objekt übertragen
     const i1 = eval(input1.value);
     const result = [...i1];
     output.publish(result);
@@ -138,6 +146,8 @@ function spread() {
 function reduce() {
     const i1 = eval(input1.value);
     const i2 = eval(input2.value);
-    const result = i1.reduce(eval(i2.value));
+    const result = i1.reduce(i2); // Assuming i2 is a valid callback function
     output.publish(result);
 }
+
+
